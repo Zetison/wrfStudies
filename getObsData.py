b52d03c790f20d08019bf681c9dc24eb2e3658f0
Jon Vegard Venås <jonvegard89@yahoo.com>
2020-08-19 18:29:59 +0200

    changes

--- getObsData.py
+++ getObsData.py
@@ -9,7 +9,7 @@ client_id = '24c65298-cf22-4c73-ad01-7c6b2c009626'
 #sourceID = "SN18700" # OSLO - BLINDERN
 #sourceID = "SN18701" # OSLO - BLINDERN PLU
 sourceID = "SN76914" # ITASMOBAWS1
-start_date = "2020-08-13T16:00:00.000Z"
+startdate = "2020-08-17T16:00:00.000Z"
 
 def getYRdata(endpoint, parameters, field):
 	# Issue an HTTP GET request
@@ -19,14 +19,12 @@ def getYRdata(endpoint, parameters, field):
 	
 	# Check if the request worked, print out any errors
 	if r.status_code == 200:
-	    data = json[field]
+	    return json[field]
 	    print('Data retrieved from frost.met.no!')
 	else:
 	    print('Error! Returned status code %s' % r.status_code)
 	    print('Message: %s' % json['error']['message'])
 	    print('Reason: %s' % json['error']['reason'])
-	
-	return data
 
 endpoint = 'https://frost.met.no/sources/v0.jsonld'
 parameters = {
@@ -40,43 +38,34 @@ data_source = getYRdata(endpoint, parameters, 'data')
 endpoint = 'https://frost.met.no/observations/v0.jsonld'
 parameters = {
     'sources': sourceID,
-    'elements': 'air_temperature',
-		'referencetime': '2020-08-13T16:00:00.000Z/2021-08-14T16:00:00.000Z',
-		'timeresolutions': 'PT10M',
+		'referencetime': startdate+'/2021-08-14T16:00:00.000Z',
+    'elements': 'air_temperature,wind_speed',
+		'timeresolutions': 'PT1H',
+		'fields': 'value, referenceTime',
 		'qualities': 0, # 0 = original value found to be good, 1 = original value suspicious (likely correct), ...
 }
-fields = ['referenceTime','value']
 
 data = getYRdata(endpoint, parameters, 'data')
 
+fields = ['referenceTime','air_temperature','wind_speed']
 # This will return a Dataframe with all of the observations in a table format
 df = pd.DataFrame()
 for i in range(len(data)):
 	row = pd.DataFrame(data[i])
 	for j in range(1,len(fields)):
-		row[fields[j]] = data[i]['observations'][fields[j]]
+		row[fields[j]] = data[i]['observations'][j-1]['value']
 	df = df.append(row)
 
 df = df.reset_index()
 
 # These additional columns will be kept
-#columns = ['sourceId','referenceTime','elementId','value','unit','timeOffset']
 df2 = df[fields].copy()
 # Convert the time value to something Python understands
 df2['referenceTime'] = pd.to_datetime(df2['referenceTime'])
 
-parameters['elements'] = 'wind_speed'
+parameters['elements'] = 'air_temperature,wind_speed'
 data = getYRdata(endpoint, parameters, 'data')
 # This will return a Dataframe with all of the observations in a table format
-df_ws = pd.DataFrame()
-for i in range(len(data)):
-	row = pd.DataFrame(data[i])
-	for j in range(1,len(fields)):
-		row[fields[j]] = data[i]['observations'][fields[j]]
-	df_ws = df_ws.append(row)
-
-df_ws = df_ws.reset_index()
-df2['wind_speed'] = df_ws['wind_speed'].copy()
 
 
 endpoint = 'https://api.met.no/weatherapi/locationforecast/2.0/complete'
@@ -90,7 +79,6 @@ data_YR = getYRdata(endpoint, parameters, 'properties')
 
 data_YR = data_YR['timeseries']
 
-
 df = pd.DataFrame()
 for i in range(len(data_YR)):
 	row = pd.DataFrame(data_YR[i])
@@ -102,12 +90,12 @@ df_YR = df[fields].copy()
 # Convert the time value to something Python understands
 df_YR['time'] = pd.to_datetime(df_YR['time'])
 fig, axs = plt.subplots(2)
-lines = axs[0].plot(df2.referenceTime,df2.value,'r',df_YR.time,df_YR.air_temperature,'b')
-lines2 = axs[1].plot(df2.referenceTime,df2.value,'r',df_YR.time,df_YR.wind_speed,'b')
+lines = axs[0].plot(df2.referenceTime,df2.air_temperature,'r',df_YR.time,df_YR.air_temperature,'b')
+lines2 = axs[1].plot(df2.referenceTime,df2.wind_speed,'r',df_YR.time,df_YR.wind_speed,'b')
 axs[0].legend(('Observated data', 'YR forecast'))
 axs[1].legend(('Observated data', 'YR forecast'))
-axs[0].set(xlabel='Time', ylabel='Temperature')
-axs[1].set(xlabel='Time', ylabel='Wind speed')
-
-
+axs[0].set(xlabel='Time', ylabel='Temperature [°C]')
+axs[1].set(xlabel='Time', ylabel='Wind speed [m/s]')
+axs[0].set_xlim(pd.to_datetime(startdate),pd.to_datetime('2020-08-21 00:00:00+00:00'))
+axs[1].set_xlim(pd.to_datetime(startdate),pd.to_datetime('2020-08-21 00:00:00+00:00'))
 plt.show()
