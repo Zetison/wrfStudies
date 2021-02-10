e484aa2ff562f4e691216c93982fdfa26f310911
Jon Vegard Venås <jonvegard89@yahoo.com>
2021-02-10 12:24:22 +0100

    Fixed bug

--- lidarValidation.py
+++ lidarValidation.py
@@ -17,7 +17,7 @@ u_inf = 10.0 # is the freestream velocity of the fluid
 p_inf = 101325 # is the static pressure in the freestream (i.e. remote from any disturbance)
 rho_inf = 1.3 # is the freestream fluid density (Air at sea level and 15 °C is 1.225kg/m^3)
 z_lnPltLvl = 0.6 # z coordinate of 1D plots
-VRADH_max = 3.0
+VRAD_max = 3.0
 SAVE_HIST = 10
 noSteps=2113
 noSteps=1584
@@ -53,11 +53,17 @@ wrfFileName = home+'/results/WRF/Frankfurt/SED_WRF_FOLDER/wrfout_d04_vol.pvd'
 lidar = PVDReader(registrationName='lidar', FileName=fileName)
 wrf = PVDReader(registrationName='wrf', FileName=wrfFileName)
 #lidar = XMLUnstructuredGridReader(registrationName='fileName', FileName=fileNames)
-lidar.CellArrays = ['VRADH']
+aziFileType = False
+if aziFileType:
+    lidarFieldName='VRADH'
+else:
+    lidarFieldName='VRAD'
+lidar.CellArrays = [lidarFieldName]
 wrf.PointArrays = ['U','V','W','P','T']
 
 cellDatatoPointData1 = CellDatatoPointData(registrationName='CellDatatoPointData1', Input=lidar)
-cellDatatoPointData1.CellDataArraytoprocess = ['VRADH']
+#cellDatatoPointData1.CellDataArraytoprocess = [lidarFieldName]
+cellDatatoPointData1.CellDataArraytoprocess = [lidarFieldName]
 # update animation scene based on data timesteps
 animationScene1.UpdateAnimationUsingDataTimeSteps()
 
@@ -79,10 +85,10 @@ calculator0.ResultArrayName = 'r'
 
 # create a new 'Calculator'
 calculator1 = Calculator(registrationName='Calculator1', Input=calculator0)
-calculator1.ResultArrayName = 'VRADH'
+calculator1.ResultArrayName = lidarFieldName
 calculator1.Function = '(U*r_X+V*r_Y+W*r_Z)/mag(r)'
-VRADHLUT = GetColorTransferFunction('VRADH')
-VRADHPWF = GetOpacityTransferFunction('VRADH')
+VRADLUT = GetColorTransferFunction(lidarFieldName)
+VRADPWF = GetOpacityTransferFunction(lidarFieldName)
 
 animationScene1.GoToLast()
 LoadPalette(paletteName='WhiteBackground')
@@ -97,7 +103,7 @@ if plotLIC:
     resampleWithDataset1 = ResampleWithDataset(SourceDataArrays=calculator1, DestinationMesh=lidar)
     resampleWithDataset1.CellLocator = 'Static Cell Locator'
     resampleWithDataset1Display = Show(resampleWithDataset1, renderView1, 'StructuredGridRepresentation')
-    resampleWithDataset1Display.ColorArrayName = ['POINTS', 'VRADH']
+    resampleWithDataset1Display.ColorArrayName = ['POINTS', lidarFieldName]
     resampleWithDataset1Display.Ambient = 1.0
     resampleWithDataset1Display.Diffuse = 0.0
 
@@ -111,14 +117,14 @@ if plotLIC:
 
     
     lidarDisplay = Show(lidar, renderView2, 'StructuredGridRepresentation')
-    #lidarDisplay.ColorArrayName = ['CELLS', 'VRADH']
+    #lidarDisplay.ColorArrayName = ['CELLS', lidarFieldName]
     lidarDisplay.Ambient = 1.0
     lidarDisplay.Diffuse = 0.0
-    ColorBy(lidarDisplay, ('CELLS', 'VRADH'))
+    ColorBy(lidarDisplay, ('CELLS', lidarFieldName))
     #resampleWithDataset = ResampleWithDataset(SourceDataArrays=cellDatatoPointData1, DestinationMesh=cone)
     #resampleWithDataset.CellLocator = 'Static Cell Locator'
     #lidarDisplay = Show(resampleWithDataset, renderView2, 'StructuredGridRepresentation')
-    #lidarDisplay.ColorArrayName = ['POINTS', 'VRADH']
+    #lidarDisplay.ColorArrayName = ['POINTS', lidarFieldName]
     #lidarDisplay.Ambient = 1.0
     #lidarDisplay.Diffuse = 0.0
 
@@ -152,21 +158,21 @@ pdo.GetFieldData().AddArray(sexaTime)'
     annotateAttributeData1Display.FontSize = 8
     #annotateTimeStep(calculator1,renderView1,'UpperLeftCorner', SAVE_HIST,color=[0.0,0.0,0.0])
 
-    VRADHLUTColorBar = GetScalarBar(VRADHLUT, renderView1)
-    VRADHLUTColorBar.Title = 'VRADH'
-    VRADHLUTColorBar.Orientation = 'Vertical'
-    VRADHLUTColorBar.WindowLocation = 'UpperRightCorner'
-    VRADHLUTColorBar.ComponentTitle = ''
-    VRADHLUTColorBar.ScalarBarLength = 0.3 
-    VRADHLUT.RescaleTransferFunction(-10.0, 10.0)
-    VRADHLUT.ApplyPreset('SINTEF1', True)
-    VRADHLUT.NanColor = [1.0, 1.0, 1.0]
+    VRADLUTColorBar = GetScalarBar(VRADLUT, renderView1)
+    VRADLUTColorBar.Title = lidarFieldName
+    VRADLUTColorBar.Orientation = 'Vertical'
+    VRADLUTColorBar.WindowLocation = 'UpperRightCorner'
+    VRADLUTColorBar.ComponentTitle = ''
+    VRADLUTColorBar.ScalarBarLength = 0.3 
+    VRADLUT.RescaleTransferFunction(-10.0, 10.0)
+    VRADLUT.ApplyPreset('SINTEF1', True)
+    VRADLUT.NanColor = [1.0, 1.0, 1.0]
     
-    VRADHLUTColorBar.WindowLocation = 'LowerCenter'
-    VRADHLUTColorBar.HorizontalTitle = 1
-    VRADHLUTColorBar.AutoOrient = 0
-    VRADHLUTColorBar.Orientation = 'Horizontal'
-    VRADHLUTColorBar.RangeLabelFormat = '%-#6.1f'
+    VRADLUTColorBar.WindowLocation = 'LowerCenter'
+    VRADLUTColorBar.HorizontalTitle = 1
+    VRADLUTColorBar.AutoOrient = 0
+    VRADLUTColorBar.Orientation = 'Horizontal'
+    VRADLUTColorBar.RangeLabelFormat = '%-#6.1f'
 
     if printLogo:
         insertSINTEFlogo(renderView2,'blue', position=[0.81,0])
@@ -175,14 +181,18 @@ pdo.GetFieldData().AddArray(sexaTime)'
     renderView1.InteractionMode = '2D'
     renderView1.CameraPosition = [466161.5742815666, 5541002.50741783, 231205.32145070643]
     renderView1.CameraFocalPoint = [466161.5742815666, 5541002.50741783, 2535.653536324522]
-    renderView1.CameraParallelScale = 17027.58881419862
+    if aziFileType:
+        cameraParallelScale = 17027
+    else:
+        cameraParallelScale = 19007
+    renderView1.CameraParallelScale = cameraParallelScale
     renderView1.ViewSize = [960,1080]
     
     renderView2.OrientationAxesVisibility = 0
     renderView2.InteractionMode = '2D'
     renderView2.CameraPosition = [466161.5742815666, 5541002.50741783, 231205.32145070643]
     renderView2.CameraFocalPoint = [466161.5742815666, 5541002.50741783, 2535.653536324522]
-    renderView2.CameraParallelScale = 17027.58881419862
+    renderView2.CameraParallelScale = cameraParallelScale
     renderView2.ViewSize = [960,1080]
     timeKeeper1 = GetTimeKeeper()
     timeKeeper1.Time = 0.0
